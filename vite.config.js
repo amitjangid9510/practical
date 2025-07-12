3989c3ef10586614206093cab5d7fa75df56663e
Amit jangid <56021794+amitjangid9510@users.noreply.github.com>
2025-07-12 13:29:45 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  server : {
-    proxy: {
-      '/api': {
-        target: 'https://fakestoreapi.com/products', 
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  },
   plugins: [react()],
 })
+
+//https://backend-virid.vercel.app/user
